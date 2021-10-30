1fe85f7066c9093532498b8c345c77b048b054bf
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-30 22:36:29 +0800

    presence

---
+++ modules/Presence.js
@@ -0,0 +1,16 @@
+module.exports = new create();
+function create(){
+    this.versions = ['1.1.1', '1.1.0'];
+
+    this.start = async (client, action, config, language) => {
+        await client.user.setPresence({
+            status: action.get(config.presence.status),
+            activities: [{
+                name: action.get(config.presence.activityName),
+                type: action.get(config.presence.type).toUpperCase()
+            }]
+        });
+
+        return true;
+    }
+}
