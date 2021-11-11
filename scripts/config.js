3b4a4d8c249c256809f5c31c8940c166fdb1ba0b
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-11-12 00:18:56 +0800

    update

--- scripts/config.js
+++ scripts/config.js
@@ -6,34 +6,55 @@ const Version = require('./version');
 const { ask } = require('fallout-utility');
 
 const commands = new Commander.Command;
-    
-    commands
-            .option('-t, --testmode')
+    commands.option('-t, --testmode');
     commands.parse();
 
-// Export
-module.exports = function() {
-    this.location = null;
-    this.config = {};
+module.exports = class {
+    /**
+     * @param {string} location config file location
+     */
+    constructor(location) {
+        this.location = location;
+        this.config = null;
+    }
 
-    this.parse = function() {
+    /**
+     * @returns {Object} returns modified config
+     */ 
+    parse() {
         if(!this.location || this.location == null) throw new Error('No config file path provided');
         let config = Fs.readFileSync(this.location, 'utf8');
             config = Yml.parse(config);
-
-        if(config.version != Version) throw new Error('Config version isn\'t compatible. Version: ' + config.version + '; Supported: ' + Version);
         
+        if(config.version != Version) throw new Error('Config version isn\'t compatible. Version: ' + config.version + '; Supported: ' + Version);
         if(config.token == 'TOKEN') config.token = null;
 
         this.config = config;
-        return config;
+        return this;
     }
-    this.prefill = function() {
-        if(!this.config.token || this.config.token == null) this.config.token = ask('Bot Token >>> ');
-    }
-    this.testmode = function() {
-        if(!commands.opts().testmode) return true;
 
-        this.config.token = process.env['discordtoken'];
+    /**
+     * @returns {Object} returns modified config
+     */
+    prefill() {
+        if(!this.config.token || this.config.token == null) this.config.token = ask('Bot Token >>> ');
+
+        return this;
+    }
+
+    /**
+     * @returns {Object} returns modified config
+     */
+    testmode() {
+        if(commands.opts().testmode) this.config.token = process.env['discordtoken'];
+
+        return this;
+    }
+
+    /**
+     * @returns {Object} returns parsed config
+     */
+    getConfig() {
+        return this.config;
     }
 }
