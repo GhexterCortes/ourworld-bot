189c6e5f4e6a66e09a5e4a742082a7616d421aaf
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-12-10 09:22:24 +0800

    sus

--- modules/message/say.js
+++ modules/message/say.js
@@ -2,12 +2,13 @@ const InteractionCommandBuilder = require('../../scripts/interactionCommandBuild
 const SafeMessage = require('../../scripts/safeMessage');
 const SafeInteract = require('../../scripts/safeInteract');
 const { replaceAll } = require('fallout-utility');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = new InteractionCommandBuilder()
     .setCommand(SlashCommandBuilder => SlashCommandBuilder
         .setName('send')
         .setDescription('Send a custom message')
-        .addSubCommand(say => say
+        .addSubcommand(say => say
             .setName('say')
             .setDescription('Says something')
             .addStringOption(text => text
@@ -21,13 +22,18 @@ module.exports = new InteractionCommandBuilder()
                 .setRequired(false)
             )
         )
-        .addSubCommand(embed => embed
+        .addSubcommand(embed => embed
             .setName('embed')
             .setDescription('Send an embed')
             .addStringOption(description => description
                 .setName('description')
                 .setDescription('The description of the embed')
                 .setRequired(true)
+            )
+            .addStringOption(reply => reply
+                .setName('reply')
+                .setDescription('Send embed reply to a message by id')
+                .setRequired(false)
             )
             .addStringOption(title => title
                 .setName('title')
@@ -40,12 +46,12 @@ module.exports = new InteractionCommandBuilder()
                 .setRequired(false)
             )
             .addStringOption(authorIcon => authorIcon
-                .setName('authorIcon')
+                .setName('author-icon')
                 .setDescription('The icon of the author of the embed')
                 .setRequired(false)
             )
             .addStringOption(authorUrl => authorUrl
-                .setName('authorUrl')
+                .setName('author-url')
                 .setDescription('The url of the author of the embed')
                 .setRequired(false)
             )
@@ -54,19 +60,9 @@ module.exports = new InteractionCommandBuilder()
                 .setDescription('The thumbnail of the embed')
                 .setRequired(false)
             )
-            .addStringOption(thumbnailUrl => thumbnailUrl
-                .setName('thumbnailUrl')
-                .setDescription('The url of the thumbnail of the embed')
-                .setRequired(false)
-            )
             .addStringOption(image => image
                 .setName('image')
                 .setDescription('The image of the embed')
-                .setRequired(false)
-            )
-            .addStringOption(imageUrl => imageUrl
-                .setName('imageUrl')
-                .setDescription('The url of the image of the embed')
                 .setRequired(false)
             )
             .addStringOption(color => color
@@ -80,17 +76,17 @@ module.exports = new InteractionCommandBuilder()
                 .setRequired(false)
             )
             .addStringOption(footerIcon => footerIcon
-                .setName('footerIcon')
+                .setName('footer-icon')
                 .setDescription('The icon of the footer of the embed')
                 .setRequired(false)
             )
             .addStringOption(footerUrl => footerUrl
-                .setName('footerUrl')
+                .setName('footer-url')
                 .setDescription('The url of the footer of the embed')
                 .setRequired(false)
             )
             .addBooleanOption(setTimestamp => setTimestamp
-                .setName('setTimestamp')
+                .setName('set-timestamp')
                 .setDescription('Set the timestamp of the embed')
                 .setRequired(false)
             )
@@ -98,11 +94,9 @@ module.exports = new InteractionCommandBuilder()
     )
     .setExecute(async (interaction, Client) => {
         this.say = async () => {
-            const text = replaceAll(interaction.options.getString('text'), '[br]', '\n');
-        
             await SafeInteract.deferReply(interaction, { ephemeral: true });
-            let reply = interaction.options.getString('reply') ? await getMessage(interaction.options.getString('reply')) : false;
-    
+            const text = replaceAll(interaction.options.getString('text'), '[br]', '\n');
+            const reply = interaction.options.getString('reply') ? await getMessage(interaction.options.getString('reply')) : false;
             const message = reply ? await SafeMessage.reply(reply, text) : await SafeMessage.send(interaction.channel, text);
     
             if(!message) {
@@ -110,13 +104,50 @@ module.exports = new InteractionCommandBuilder()
             } else {
                 await SafeInteract.editReply(interaction, 'Message sent!');
             }
-    
-            async function getMessage(id) {
-                const foundMessage = await interaction.channel.messages.fetch(id).catch(() => false);
-    
-                if(!foundMessage) await interaction.editReply('I could not find the reply message id!');
-    
-                return foundMessage ? foundMessage : false;
+        }
+        this.embed = async () => {
+            await SafeInteract.deferReply(interaction, { ephemeral: true });
+            const description = replaceAll(interaction.options.getString('description'), '[br]', '\n');
+            const title = interaction.options.getString('title');
+            const author = interaction.options.getString('author');
+            const authorIcon = interaction.options.getString('author-icon');
+            const authorUrl = interaction.options.getString('author-url');
+            const thumbnail = interaction.options.getString('thumbnail');
+            const image = interaction.options.getString('image');
+            const color = interaction.options.getString('color');
+            const footer = interaction.options.getString('footer');
+            const footerIcon = interaction.options.getString('footer-icon');
+            const footerUrl = interaction.options.getString('footer-url');
+            const setTimestamp = interaction.options.getBoolean('set-timestamp');
+
+            const embed = new MessageEmbed().setDescription(description);
+
+            if(title) embed.setTitle(title);
+            if(author) embed.setAuthor(author, (authorIcon ? authorIcon : null), (authorUrl ? authorUrl : null));
+            if(thumbnail) embed.setThumbnail(thumbnail);
+            if(image) embed.setImage(image);
+            if(color) embed.setColor(color);
+            if(footer) embed.setFooter(footer, (footerIcon ? footerIcon : null), (footerUrl ? footerUrl : null));
+            if(setTimestamp) embed.setTimestamp();
+
+            const reply = interaction.options.getString('reply') ? await getMessage(interaction.options.getString('reply')) : false;
+            const message = reply ? await SafeMessage.reply(reply, { content: ' ', embeds: [embed] }) : await SafeMessage.send(interaction.channel, { content: ' ', embeds: [embed] });
+
+            if(!message) {
+                await SafeInteract.editReply(interaction, 'I could not send the embed!');
+            } else {
+                await SafeInteract.editReply(interaction, 'Embed sent!');
             }
+        }
+
+        const command = interaction.options.getSubcommand();
+        if(command && this.hasOwnProperty(command)) await this[command]();
+
+        async function getMessage(id) {
+            const foundMessage = await interaction.channel.messages.fetch(id).catch(() => false);
+
+            if(!foundMessage) await interaction.editReply('I could not find the reply message id!');
+
+            return foundMessage ? foundMessage : false;
         }
     });
