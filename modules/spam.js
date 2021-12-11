5386f521d679e58a3a45bbe0494c91812bb02ba3
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-12-12 02:15:55 +0800

    spam pings

---
+++ modules/spam.js
@@ -0,0 +1,26 @@
+const MemberPermission = require('../scripts/memberPermissions');
+const SafeMessage = require('../scripts/safeMessage');
+
+class Spammer {
+    constructor() {
+        this.versions = ['1.4.2'];
+    }
+
+    async start(Client) {
+        Client.on('message', async (message) => {
+            if (message.author.bot) return;
+            if (message && MemberPermission.admin(message.member)) return;
+
+            // count message pings
+            if((message.content.toLowerCase().includes('pls') || message.content.toLowerCase().includes('owo')) && message.mentions.users.size > 0) {
+                const reply = await SafeMessage.reply(message, 'Avoid pinging people when using bots!');
+
+                setTimeout(async () => SafeMessage.delete(reply), 5000);
+            }
+        });
+        
+        return true;
+    }
+}
+
+module.exports = new Spammer();
