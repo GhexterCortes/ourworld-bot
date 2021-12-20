0395f39aa2aa6dc0aea9497c6571a9ec70c2ed45
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-12-20 20:35:11 +0800

    Update spam.js

--- modules/spam.js
+++ modules/spam.js
@@ -1,5 +1,6 @@
 const MemberPermission = require('../scripts/memberPermissions');
 const SafeMessage = require('../scripts/safeMessage');
+const { MessageEmbed } = require('discord.js');
 
 class Spammer {
     constructor() {
@@ -7,12 +8,42 @@ class Spammer {
     }
 
     async start(Client) {
+        const config = Client.AxisUtility.getConfig();
         Client.on('messageCreate', async (message) => {
-            if (message.author.bot) return;
-            if (message && MemberPermission.admin(message.member)) return;
+            if (!message || message.author.bot) return;
+
+            const everyone = message.mentions?.everyone;
+            const here = message.mentions?.here;
+
+            if (everyone || here) {
+                const reply = await SafeMessage.reply(message, { content: ' ', embeds: [
+                    new MessageEmbed()
+                        .setColor(config.embedColor)
+                        .setAuthor('Mass mentions detected!', message.author.avatarURL({ format: 'png', dynamic: true, size: 1024 }))
+                        .setDescription(`To respect people's peace, avoid mass mention @everyone or @here.\n\`\`\`You may review the rules if you are a member of the staff.\`\`\`\n\n`)
+                        .setFooter(`The owner received the original copy of this message for review.`)
+                        .setTimestamp()
+                ] });
+
+
+                // get guild from message
+                const ownerId = await Client.guilds.cache.get(message.guildId)?.ownerId;
+                const owner = ownerId ? await Client.users.fetch(ownerId) : null;
+
+                console.log(owner);
+                if(owner) await SafeMessage.send(owner, {
+                    content: 'This message was marked as spam! by ' + message.author.tag,
+                    embeds: [
+                        new MessageEmbed()
+                            .setColor(config.embedColor)
+                            .setDescription(message.content)
+                    ]
+                });
+                setTimeout(async () => SafeMessage.delete(message), 5000);
+            }
 
             // count message pings
-            if((message.content.toLowerCase().startsWith('pls ') || message.content.toLowerCase().startsWith('owo ')) && message.mentions.users.size > 0) {
+            if(MemberPermission.admin(message.member) && (message.content.toLowerCase().startsWith('pls ') || message.content.toLowerCase().startsWith('owo ')) && message.mentions.users.size > 0) {
                 const reply = await SafeMessage.reply(message, ':no_entry_sign: Avoid pinging people when using bots this can lead to spam!\nYou can try using user id instead of @user\n```\nRight click the user profile then click Copy ID\n```\nThis method doesn\'t always work');
 
                 //setTimeout(async () => SafeMessage.delete(reply), 5000);
