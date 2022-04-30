938a7529cf14883027cd3fbe9883b562473c6994
GhexterCortes <ghextershumies@gmail.com>
2022-04-30 17:02:12 +0800

    banned words

---
+++ src/bannedwords-mc.ts
@@ -0,0 +1,129 @@
+import { Logger, replaceAll } from 'fallout-utility';
+import yml from 'yaml';
+import path from 'path';
+import { RecipleClient, RecipleScript, version } from 'reciple';
+import { createConfig } from './_createConfig';
+import { TextChannel } from 'discord.js';
+import stringSimilarity from 'string-similarity-js';
+import { weirdToNormalChars } from 'weird-to-normal-chars';
+
+export interface BannedWordsMCConfig {
+    bannedWords: {
+        word: string;
+        command: string;
+    }[],
+    executedCommandLimit: number,
+    chatUsernameSeparator: string,
+    consoleBotIds: string[],
+    gameChannels: string[],
+    consoleChannels: string[],
+    similarWordThreshold: number
+}
+
+class BannedWordsMC implements RecipleScript {
+    public versions: string[] = [version];
+    public client?: RecipleClient;
+    public logger: Logger = new Logger('BannedWords-MC');
+    public config = BannedWordsMC.getConfig();
+    public consoleChannels: TextChannel[] = [];
+
+    public onStart(client: RecipleClient) {
+        this.client = client;
+        this.logger = this.client.logger.cloneLogger();
+        this.logger.defaultPrefix = 'BannedWords-MC';
+
+        this.logger.info('Loaded banned words for minecraft');
+
+        return true;
+    }
+
+    public async onLoad() {
+        this.logger.debug('Loading console channels');
+
+        for (const channelId of this.config.consoleChannels) {
+            const channel = this.client?.channels.cache.find(c => c.id === channelId && c.type == 'GUILD_TEXT') ?? await this.client?.channels.fetch(channelId).then(c => c?.type == 'GUILD_TEXT' ? c : undefined).catch(() => undefined) ?? undefined;
+            if (!channel) continue;
+
+            this.consoleChannels.push(channel as TextChannel);
+            this.logger.debug(`Loaded console channel ${channel.id}`);
+        }
+
+        this.client?.on('messageCreate', async message => {
+            if (!message.author.bot || !this.config.consoleBotIds.includes(message.author.id)) return;
+            if (!this.config.gameChannels.includes(message.channelId)) return;
+
+            const data = this.separateUsernameChat(message.content);
+            if (!data.author) return;
+
+            const check = BannedWordsMC.checkExplicity(weirdToNormalChars(data.message).split(' '), this.config.bannedWords, this.config.similarWordThreshold);
+
+            if (!check.words.length) return;
+
+            const commands = check.commands.map(c => c ? replaceAll(c, '%player%', data.author) : c).splice(0, this.config.executedCommandLimit);
+
+            this.logger.debug(`${data.author} used banned words: ${check.words.join(', ')}`);
+
+            for (const channel of this.consoleChannels) {
+                message.channel.send(`**${data.author}** used banned ${check.words.length > 1 ? 'words' : 'word'}: ||${check.words.join('|| ||')}||`).catch(err => this.logger?.error(err));
+                
+                for (const command of commands) {
+                    if (!command) { continue; }
+                    await channel.send(command).catch(err => this.logger?.error(err));
+                }
+            }
+        });
+    }
+
+    public separateUsernameChat(message: string): { author: string; message: string; } {
+        const split = message.split(this.config.chatUsernameSeparator);
+        if (split.length < 2) return { author: '', message: BannedWordsMC.clean(message) };
+
+        const author = split.shift()?.split(' ').shift() ?? '';
+        const msg = split.pop() ?? '';
+
+        return { author, message: BannedWordsMC.clean(msg) };
+    }
+
+    public static checkExplicity(words: string[], explicitWords: BannedWordsMCConfig['bannedWords'], stringSimilarityThreshold: number = 0.6): { words: string[], commands: string[] } {
+        let commands: string[] = [];
+
+        const matched = explicitWords.filter(word => {
+            const match = words.includes(word.word.toLowerCase()) ||
+                words.some(w => stringSimilarity(word.word, w) >= stringSimilarityThreshold);
+
+            if (!match) return false;
+
+            commands.push(word.command);
+
+            return true;
+        });
+
+        return { words: matched.map(w => w.word), commands };
+    }
+
+    public static clean(str: string): string {
+        return str.replace(/[^\w\s]/gi, '');
+    }
+
+    public static getConfig(): BannedWordsMCConfig {
+        const configPath = path.join(process.cwd(), 'config/bannedwords-mc/config.yml');
+        const defaultConfig: BannedWordsMCConfig = {
+            bannedWords: [
+                {
+                    word: 'bitch',
+                    command: 'warn %player% bad language'
+                }
+            ],
+            executedCommandLimit: 1,
+            chatUsernameSeparator: '>',
+            consoleBotIds: ['000000000000000000', '000000000000000000'],
+            gameChannels: ['000000000000000000', '000000000000000000'],
+            consoleChannels: ['000000000000000000', '000000000000000000'],
+            similarWordThreshold: 0.6
+        };
+
+        return yml.parse(createConfig(configPath, defaultConfig));
+    }
+}
+
+module.exports = new BannedWordsMC();
---
+++ src/bannedwords.ts
@@ -0,0 +1,122 @@
+import { Logger } from 'fallout-utility';
+import path from 'path';
+import yml from 'yaml';
+import stringSimilarity from 'string-similarity-js';
+import { weirdToNormalChars } from 'weird-to-normal-chars';
+import { isIgnoredChannel, RecipleClient, RecipleScript, version } from 'reciple';
+import { createConfig } from './_createConfig';
+import { MessageEmbed } from 'discord.js';
+
+export interface BannedWordsConfig {
+    bannedWords: {
+        word: string;
+        punishment: 'kick' | 'ban' | 'mute' | '';
+    }[],
+    similarWordThreshold: number
+}
+
+export class BannedWords implements RecipleScript {
+    public versions: string[] = [version];
+    public client?: RecipleClient;
+    public logger: Logger = new Logger('BannedWords');
+    public config: BannedWordsConfig = BannedWords.getConfig();
+
+    public onStart(client: RecipleClient) {
+        this.client = client;
+        this.logger = this.client.logger.cloneLogger();
+        this.logger.defaultPrefix = 'BannedWords';
+
+        this.logger.info('Loaded banned words');
+
+        return true;
+    }
+
+    public onLoad() {
+        this.client?.on('messageCreate', async message => {
+            if (message.author.bot || message.author.system) return;
+            if (isIgnoredChannel(message.channelId, this.client!.config!.ignoredChannels)) return;
+
+            const words = weirdToNormalChars(message.content).toLowerCase().split(' ');
+            const explicitWords = BannedWords.checkExplicity(words, this.config.bannedWords, this.config.similarWordThreshold);
+
+            if (!explicitWords.words.length) return;
+
+            this.logger.debug(`${message.author.username} (${message.author.id}) in ${message.channelId} said: ${message.content}`);
+
+            message.delete().catch(() => {});
+
+            if (!explicitWords.punishment) return;
+
+            const embed = new MessageEmbed()
+                .setAuthor({ name: `Banned ${explicitWords.words.length > 1 ? 'words' : 'word'}` })
+                .setDescription(`||\`${explicitWords.words.join('`|| ||`')}\`||`)
+                .addField('Punishment', `\`${explicitWords.punishment.toUpperCase()}\``)
+                .setColor('RED');
+
+                message.channel.send({ embeds: [embed] }).catch(() => {});
+
+            switch (explicitWords.punishment) {
+                case 'kick':
+                    if (!message.member?.kickable) break;
+                    await message.member?.kick().catch(err => this.logger.error(err));
+                    break;
+                case 'ban':
+                    if (!message.member?.bannable) break;
+                    await message.member?.ban().catch(err => this.logger.error(err));
+                    break;
+                case 'mute':
+                    if (message.member?.isCommunicationDisabled()) break;
+                    await message.member?.timeout(1000 * 60 * 1).catch(err => this.logger.error(err));
+                    break;
+            }
+        });
+    }
+
+    public static checkExplicity(words: string[], explicitWords: BannedWordsConfig['bannedWords'], stringSimilarityThreshold: number = 0.6): { words: string[], punishment: string } {
+        let punishment: string = '';
+
+        words = words.map(w => this.clean(w));
+
+        const matched = explicitWords.filter(word => {
+            word.word = this.clean(word.word);
+
+            const match = words.includes(word.word.toLowerCase()) ||
+                words.some(w => stringSimilarity(word.word, w) >= stringSimilarityThreshold);
+
+            if (!match) return false;
+
+            switch (word.punishment) {
+                case 'ban':
+                    if (punishment == 'kick' || punishment == 'mute' || !punishment) punishment = 'ban';
+                    break;
+                case 'kick':
+                    if (punishment == 'mute' || !punishment) punishment = 'kick';
+                    break;
+                case 'mute':
+                    if (!punishment) punishment = 'mute';
+            }
+
+            return true;
+        });
+
+        return { words: matched.map(w => w.word), punishment };
+    }
+
+    public static clean(str: string): string {
+        return str.replace(/[^\w\s]/gi, '');
+    }
+
+    public static getConfig(): BannedWordsConfig {
+        const configPath = path.join(process.cwd(), 'config/bannedwords/config.yml');
+        const defaultConfig: BannedWordsConfig = {
+            bannedWords: [
+                { word: 'bitch', punishment: 'mute' }
+            ],
+            similarWordThreshold: 0.6
+        }; 
+
+        return yml.parse(createConfig(configPath, defaultConfig));
+    }
+}
+
+module.exports = new BannedWords();
